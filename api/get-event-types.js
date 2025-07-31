4ebb4c20eb5034a84062acf99ff7af139d1eac90
umaiiransarii123 <umair@dartmarketing.io>
2025-07-31 18:02:55 +0500

    Update get-event-types.js

--- api/get-event-types.js
+++ api/get-event-types.js
@@ -1,22 +1,12 @@
 const fetch = require('node-fetch');
 
 module.exports = async (req, res) => {
-  const userResponse = await fetch("https://api.calendly.com/users/me", {
+  const response = await fetch("https://api.calendly.com/event_types", {
     headers: {
       Authorization: `Bearer ${process.env.CALENDLY_API_KEY}`,
     },
   });
 
-  const userData = await userResponse.json();
-  const userUri = userData.resource.uri;
-
-  const eventTypesResponse = await fetch(`https://api.calendly.com/event_types?user=${userUri}`, {
-    headers: {
-      Authorization: `Bearer ${process.env.CALENDLY_API_KEY}`,
-    },
-  });
-
-  const eventTypesData = await eventTypesResponse.json();
-
-  res.status(200).json(eventTypesData.collection);
+  const data = await response.json();
+  res.status(200).json(data || {});
 };
